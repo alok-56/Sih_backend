6ed7ee3138f82a27e0bb2939f360727b302c1a30
alok-56 <alokkumar11746@gmail.com>
2023-12-09 19:33:44 +0530

    alok

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
-// const morgan = require("morgan");
+const cors = require("cors");
+const morgan = require("morgan");
 const DbConnect = require("./Config/Db");
+const DummyRouter = require("./Routes/DummyData");
+const globalErrHandler = require("./Middleware/GlobalError");
+const UserRouter = require("./Routes/Users");
+const CollegeRouter = require("./Routes/College");
 DbConnect();
-// const DummyRouter = require("./Routes/DummyData");
-// const globalErrHandler = require("./Middleware/GlobalError");
-// const UserRouter = require("./Routes/Users");
-// const CollegeRouter = require("./Routes/College");
-// const UserModel = require("./Model/User");
 
-// //------------------Middleware-------------------//
 
-// app.use(express.json());
-// app.use(cors());
-// app.use(morgan("dev"));
+//------------------Middleware-------------------//
 
-// //------------------Roues Middleware----------------//
-// app.use("/api/v1/DummyData", DummyRouter);
-// app.use("/api/v1/Users", UserRouter);
-// app.use("/api/v1/College", CollegeRouter);
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//------------------Roues Middleware----------------//
+app.use("/api/v1/DummyData", DummyRouter);
+app.use("/api/v1/Users", UserRouter);
+app.use("/api/v1/College", CollegeRouter);
 
 // app.get("/", async (req, res) => {
 //   let user = await UserModel.find();
@@ -28,7 +28,7 @@ DbConnect();
 //   });
 // });
 
-// app.use(globalErrHandler);
+app.use(globalErrHandler);
 
 const port =process.env.PORT || 8080 
 app.listen(port, () => {

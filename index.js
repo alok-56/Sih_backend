43e1166ceca79b6bb378f50454bce66ec420c2f5
alok-56 <alokkumar11746@gmail.com>
2023-12-10 00:27:40 +0530

    alok

--- index.js
+++ index.js
@@ -7,24 +7,25 @@ const DummyRouter = require("./Routes/DummyData");
 const globalErrHandler = require("./Middleware/GlobalError");
 const UserRouter = require("./Routes/Users");
 const CollegeRouter = require("./Routes/College");
+const bodyParser = require("body-parser");
 DbConnect();
-
 
 //------------------Middleware-------------------//
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //------------------Roues Middleware----------------//
 app.use("/api/v1/DummyData", DummyRouter);
 app.use("/api/v1/Users", UserRouter);
 app.use("/api/v1/College", CollegeRouter);
 
-
 app.use(globalErrHandler);
 
-const port =process.env.PORT || 8080 
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listen on port ${port}`);
 });
